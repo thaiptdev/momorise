da516ec41d8aabdd1106b0bdae312258ae4d4ebd
Phạm Thành Thái <thaipt.dev@gmail.com>
2025-07-04 11:27:48 +0700

    update painting title

--- src/data/paintingData.ts
+++ src/data/paintingData.ts
@@ -10,21 +10,21 @@ export const paintingData: Record<
       "Under the golden sunlight, the daughter felt a deep connection to her mother for the first time. In that one second, she suddenly realised how close her mother was to her, not as far away as she thought before. As the day is about to end, something new grows gradually: a bond between mother and daughter.\n\nPainted in the daughter’s point of view, this painting shows the mother smiling happily, reaching her hand as if she wanted to invite us to view the sunset with her, suggesting a willingness to communicate and understand from the mother.",
   },
   "2": {
-    title: "Whispers in the Wind",
+    title: "The Tender Warmth",
     author: "Tue Anh",
     image: "/images/main_painting.png",
     content:
       "Under the golden sunlight, the daughter felt a deep connection to her mother for the first time. In that one second, she suddenly realised how close her mother was to her, not as far away as she thought before. As the day is about to end, something new grows gradually: a bond between mother and daughter.\n\nPainted in the daughter’s point of view, this painting shows the mother smiling happily, reaching her hand as if she wanted to invite us to view the sunset with her, suggesting a willingness to communicate and understand from the mother.",
   },
   "3": {
-    title: "Echoes of the Past",
+    title: "The Tender Warmth",
     author: "Tue Anh",
     image: "/images/main_painting.png",
     content:
       "Under the golden sunlight, the daughter felt a deep connection to her mother for the first time. In that one second, she suddenly realised how close her mother was to her, not as far away as she thought before. As the day is about to end, something new grows gradually: a bond between mother and daughter.\n\nPainted in the daughter’s point of view, this painting shows the mother smiling happily, reaching her hand as if she wanted to invite us to view the sunset with her, suggesting a willingness to communicate and understand from the mother.",
   },
   "4": {
-    title: "The Unspoken Bond",
+    title: "The Tender Warmth",
     author: "Tue Anh",
     image: "/images/main_painting.png",
     content:
